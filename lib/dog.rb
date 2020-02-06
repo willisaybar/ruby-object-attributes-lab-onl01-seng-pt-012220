fcddfb6cf5ab68e0839e7028976ee0155e955c83
Willis Aybar <willisaybar@gmail.com>
2020-02-06 01:39:56 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+ 
+  def dog_name(name)
+    @name = name
+  end
+ 
+  def name
+    @name
+  end
+  
+  def name=(new_name)
+    @name = new_name
+  end
+ 
+  def breed=(breed)
+    @breed = breed
+  end
+ 
+  def breed
+    @breed
+  end
+ 
+end
