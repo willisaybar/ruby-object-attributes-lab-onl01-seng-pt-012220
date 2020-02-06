300d0722293656e552e54c382b8ccfee7190ba6e
Willis Aybar <willisaybar@gmail.com>
2020-02-06 01:14:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
-class Dog 
-  def dog=(dog name)
-    
+class Dog
+ 
+  def dog_name(name)
+    @name = name
+  end
+ 
+  def name
+    @name
+  end
+ 
+  def name=(new_name)
+    @name = new_name
+  end
+ 
+  def beed=(new_breed)
+    @breed = new_breed
+ 
+end
